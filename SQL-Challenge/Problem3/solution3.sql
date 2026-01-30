7fa97c9c91a4dae8159e2b58cdfa5ad00b112e4f
krishnad03 <dubeyk7649@gmail.com>
2026-01-30 15:34:56 +0530

    Complete Assessment solutions - Solved 5 SQL challenges and 1 python challenge

--- SQL-Challenge/Problem3/solution3.sql
+++ SQL-Challenge/Problem3/solution3.sql
@@ -1,1 +1,12 @@
 -- write your solution here
+#output1
+with cte1 as(
+select test_id, marks, lag(marks,1,0) over(order by test_id ) as prevm  from student_marks
+)
+select test_id, marks from cte1 where marks>prevm
+
+#output2
+with cte1 as(
+select test_id, marks, lag(marks) over(order by test_id ) as prevm  from student_marks
+)
+select test_id, marks from cte1 where marks>prevm
