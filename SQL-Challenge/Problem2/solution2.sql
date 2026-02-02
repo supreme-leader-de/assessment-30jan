354f85e2a507352e7503f4f19e9a501eac9287df
TanmayIndoriya <tanmayindoriya@gmail.com>
2026-02-02 11:22:56 +0530

    missing queries

--- SQL-Challenge/Problem2/solution2.sql
+++ SQL-Challenge/Problem2/solution2.sql
@@ -1,1 +1,36 @@
 -- write your solution here
+
+with income_cte as (
+	select s.emp_id, s.emp_name, i.income,
+		   s.base_salary * i.percentage/ 100 as amount
+	from salary s
+    cross join income i
+),
+deduction_cte as (
+	select s.emp_id, s.emp_name, d.deduction,
+		   s.base_salary * d.percentage/ 100 as amount
+	from salary s
+    cross join deduction d
+)
+select
+	i.emp_name as employee,
+
+    sum(case when i.income = 'Basic' then i.amount end) as basic,
+    sum(case when i.income = 'Allowance' then i.amount end) as allowance,
+	sum(case when i.income = 'Others' then i.amount end) as "others",
+    sum(case when i.income in ('Basic','Allowance','Others') then i.amount end) as 'gross salary',
+
+    sum(case when d.deduction = 'Insurance' then d.amount end) as insurance,
+    sum(case when d.deduction = 'Health' then d.amount end) as health,
+    sum(case when d.deduction = 'House' then d.amount end) as house,
+    sum(case when d.deduction in ('Insurance','Health','House') then d.amount end) as 'total deductions',
+
+	((sum(case when i.income in ('Basic','Allowance','Others') then i.amount end))
+	-
+    (sum(case when d.deduction in ('Insurance','Health','House') then d.amount end)))
+    as 'net pay'
+
+    from income_cte i
+    join deduction_cte d
+    on i.emp_id = d.emp_id
+    group by i.emp_name;
--- SQL-Challenge/Problem4/solution4.sql
+++ SQL-Challenge/Problem4/solution4.sql
@@ -1,1 +1,5 @@
 -- write your solution here
+
+select *
+from day_indicator
+where substring(day_indicator, weekday(dates), 1) = weekday(dates);
